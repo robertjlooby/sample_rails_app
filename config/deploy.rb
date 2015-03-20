76e73eab5a6d8ef350b675ff14b888a2e4fd58ff
Rob Looby <robertjlooby@gmail.com>
2015-03-20 14:29:51 -0500

    add default cap config

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'sample_rails_app'
+set :repo_url, 'git@github.com:robertjlooby/sample_rails_app.git'
+
+set :deploy_to, '/var/www/sample_rails_app'
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
