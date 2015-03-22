59aaa888aea4f4b4fa480a69aa048c0a4f138c30
Rob Looby <robertjlooby@gmail.com>
2015-03-22 17:21:37 -0500

    add shared database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:robertjlooby/sample_rails_app.git'
 set :deploy_to, '/var/www/sample_rails_app'
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
